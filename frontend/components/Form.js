dc6054c3fdc1bb6db2d83e2259ede95cbe2da619
babotavo419 <one.faith419@gmail.com>
2023-06-26 07:24:21 -0400

    checkIfInputsAreValid/adduseEffect/SeleinfoMessage

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { postQuiz, setMessage, resetForm } from '../state/action-creators';
 
 export default function Form() {
   const dispatch = useDispatch();
+  const infoMessage = useSelector(state => state.infoMessage);
 
   const [newQuestion, setNewQuestion] = useState('');
   const [newTrueAnswer, setNewTrueAnswer] = useState('');
@@ -11,15 +12,30 @@ export default function Form() {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   useEffect(() => {
+    const savedState = localStorage.getItem('formData');
+    if (savedState) {
+      const parsedState = JSON.parse(savedState);
+      setNewQuestion(parsedState.newQuestion || '');
+      setNewTrueAnswer(parsedState.newTrueAnswer || '');
+      setNewFalseAnswer(parsedState.newFalseAnswer || '');
+    }
+  }, []);
+
+  useEffect(() => {
     const checkIfInputsAreValid = () => {
       return (
-        newQuestion && newQuestion.trim() &&
-        newTrueAnswer && newTrueAnswer.trim() &&
-        newFalseAnswer && newFalseAnswer.trim()
+        newQuestion.trim().length > 1 &&
+        newTrueAnswer.trim().length > 1 &&
+        newFalseAnswer.trim().length > 1
       );
     };
 
     setIsButtonDisabled(!checkIfInputsAreValid());
+  }, [newQuestion, newTrueAnswer, newFalseAnswer]);
+
+  useEffect(() => {
+    const stateToSave = { newQuestion, newTrueAnswer, newFalseAnswer };
+    localStorage.setItem('formData', JSON.stringify(stateToSave));
   }, [newQuestion, newTrueAnswer, newFalseAnswer]);
 
   const handleInputChange = (event) => {
@@ -41,13 +57,24 @@ export default function Form() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(postQuiz(newQuestion, newTrueAnswer, newFalseAnswer));
+    dispatch(postQuiz(newQuestion, newTrueAnswer, newFalseAnswer))
+      .then(() => {
+        dispatch(setMessage('Quiz successfully added!'));
+        setNewQuestion('');
+        setNewTrueAnswer('');
+        setNewFalseAnswer('');
+      })
+      .catch(() => {
+        dispatch(setMessage('Error submitting new quiz.'));
+      });
   };
   
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Add a New Quiz</h2>
+      
+      {infoMessage && <div>{infoMessage}</div>}
       
       <div>
         <label htmlFor="newQuestion">Question: </label>
