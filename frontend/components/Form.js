6eb94954287957efd0d8c5c19e90a270953accc7
babotavo419 <one.faith419@gmail.com>
2023-06-27 05:44:06 -0400

    ok

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,65 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { postQuiz } from '../state/action-creators';
+import { postQuiz, inputChange } from '../state/action-creators';
 
 export default function Form() {
   const dispatch = useDispatch();
   const formState = useSelector(state => state.form);
-
-  const { newQuestion, newTrueAnswer, newFalseAnswer } = formState; // Access form fields from the Redux store
-
+  
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    switch (name) {
-      case 'newQuestion':
-        setNewQuestion(value);
-        break;
-      case 'newTrueAnswer':
-        setNewTrueAnswer(value);
-        break;
-      case 'newFalseAnswer':
-        setNewFalseAnswer(value);
-        break;
-      default:
-        break;
-    }
+    dispatch(inputChange(name, value));
   };
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    const { newQuestion, newTrueAnswer, newFalseAnswer } = formState;
     dispatch(postQuiz(newQuestion, newTrueAnswer, newFalseAnswer));
   };
-
-  const isButtonDisabled = newQuestion.trim().length <= 1 || newTrueAnswer.trim().length <= 1 || newFalseAnswer.trim().length <= 1;
-
-  const setNewQuestion = (value) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      fieldName: 'newQuestion',
-      value: value,
-    });
-  };
-
-  const setNewTrueAnswer = (value) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      fieldName: 'newTrueAnswer',
-      value: value,
-    });
-  };
-
-  const setNewFalseAnswer = (value) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      fieldName: 'newFalseAnswer',
-      value: value,
-    });
-  };
-
+  
+  const { newQuestion, newTrueAnswer, newFalseAnswer } = formState;
+  
+  const isButtonDisabled = newQuestion.length <= 1 || newTrueAnswer.length <= 1 || newFalseAnswer.length <= 1;
+  
   return (
     <form onSubmit={handleSubmit}>
       <h2>Create New Quiz</h2>
-
+  
       <div>
         <label htmlFor="newQuestion">Question: </label>
         <input
@@ -70,7 +35,7 @@ export default function Form() {
           onChange={handleInputChange}
         />
       </div>
-
+  
       <div>
         <label htmlFor="newTrueAnswer">True Answer: </label>
         <input
@@ -81,7 +46,7 @@ export default function Form() {
           onChange={handleInputChange}
         />
       </div>
-
+  
       <div>
         <label htmlFor="newFalseAnswer">False Answer: </label>
         <input
@@ -92,7 +57,7 @@ export default function Form() {
           onChange={handleInputChange}
         />
       </div>
-
+  
       <button
         type="submit"
         disabled={isButtonDisabled}
@@ -104,4 +69,5 @@ export default function Form() {
     </form>
   );
 }
+
 
