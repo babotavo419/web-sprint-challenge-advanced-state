f870e69ee0792996728d5506b043e5ed2992bf99
babotavo419 <one.faith419@gmail.com>
2023-06-27 03:41:20 -0400

    deleted unused imports

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { postQuiz, infoMessage, resetForm } from '../state/action-creators';
+import { postQuiz } from '../state/action-creators';
 
 export default function Form() {
   const dispatch = useDispatch();
-  const infoMessage = useSelector(state => state.infoMessage);
   const formState = useSelector(state => state.form); // Listen for the form state in Redux
 
   const [newQuestion, setNewQuestion] = useState('');
